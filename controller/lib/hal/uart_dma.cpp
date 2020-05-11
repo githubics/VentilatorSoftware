d161b2e9bcdcae3a9b648e44115148717a948fdf
miceuz <mic@hardcore.lt>
2020-05-11 09:39:58 +0300

    Add an abstract listener class, call listener methods on events. #159

--- controller/lib/hal/uart_dma.cpp
+++ controller/lib/hal/uart_dma.cpp
@@ -8,16 +8,43 @@
 // endpoints as peripheral and memory. Upon DMA transfer completion
 // CPU is notified via interrupt.
 
-class UART3_DMA {
+// This driver also provides Character Match interrupt on reception.
+// UART is setup to issue an interrupt upot the reception of the
+// specified character.
+// NOTE: interrupt is disabled after the first match, call
+// interruptOnChar to set up a an interrupt again.
+
+// An interface that gets called back by the driver on rx, tx complete
+// and rx character match events.
+// NOTE: all callbacks are called from interrupt context!
+class UART_DMA_Listener {
 public:
+  // Called on DMA RX complete
+  virtual void onRxComplete() = 0;
+  // Called on DMA TX complete
+  virtual void onTxComplete() = 0;
+  // Called on specified character reception
+  virtual void onCharacterMatch() = 0;
+};
+
+class UART3_DMA {
   UART_Regs *const uart;
   DMA_Regs *const dma;
+  UART_DMA_Listener &eventListener;
 
+public:
   bool tx_in_progress;
   bool rx_in_progress;
 
-  UART3_DMA() : uart(UART3_BASE), dma(DMA1_BASE) {}
+  // NOTE: UART3 is hardcoded here because RespiraWorks cycle controller
+  // uses UART3 to communicate with Raspberry Pi.
+  // Class is not generic to avoid excessive code while resolving
+  // DMA channel numbers that have to be set up.
+  // Specifically USART3_TX is on DMA channel 2 and USART3_RX on channel 3.
+  UART3_DMA(UART_DMA_Listener &listener)
+      : uart(UART3_BASE), dma(DMA1_BASE), eventListener(listener) {}
 
+  // Performs basic UART3 initialization
   void init(int baud) {
     // Set baud rate register
     uart->baud = CPU_FREQ / baud;
@@ -27,17 +54,23 @@ public:
     // TODO Enable Hardware flow controll
   }
 
+  // Returns true if DMA TX is in progress
   bool isTxInProgress() {
     // TODO thread safety
     return tx_in_progress;
   }
 
+  // Returns true if DMA RX is in progress
   bool isRxInProgress() {
     // TODO thread safety
     return rx_in_progress;
   }
 
-  bool transmit(const char *buf, uint32_t length) {
+  // Sets up UART3 to transfer [length] characters from [buf]
+  // Returns false if DMA transmission is in progress, does not
+  // interrupt previous transmission.
+  // Returns true if no transmission is in progress
+  bool startTX(const char *buf, uint32_t length) {
     if (isTxInProgress()) {
       return false;
     }
@@ -49,8 +82,8 @@ public:
     dma->channel[1].config.msize = 0;       // 8 bits
     dma->channel[1].config.psize = 0;       // 8 bits
     dma->channel[1].config.memInc = 1;      // increment source (memory) address
-    dma->channel[1].config.perInc =
-        0; // don't increment dest (peripheral) address
+    dma->channel[1].config.perInc = 0;      // don't increment dest (peripheral)
+                                            // address
     dma->channel[1].config.circular = 0; // not circular
     dma->channel[1].config.dir = 1;      // memory to peripheral
     dma->channel[1].config.teie = 1;     // interrupt on error
@@ -71,15 +104,11 @@ public:
     return true;
   }
 
-  void interruptOnChar(char c) {
-    uart->ctrl[0] =
-        0x0008; // TODO Disable reader in order to enable characted detection
-                //    uart->ctrl[1].addr = stopChar; // TODO
-    uart->ctrl[0] = 0x000D; // TODO enable receiver and Character match
-                            // interrupt
-  }
+  // Sets up reception of at least [length] chars from UART3 into [buf]
+  // Returns false if reception is in progress, new reception is not
+  // setup. Returns true if no reception is in progress.
 
-  bool setupDmaRx(const char *buf, const uint32_t length) {
+  bool startRX(const char *buf, const uint32_t length) {
     // UART3 reception happens on DMA1 channel 3
     if (isRxInProgress()) {
       return false;
@@ -91,8 +120,8 @@ public:
     dma->channel[2].config.msize = 0b00;    // 8 bits
     dma->channel[2].config.psize = 0b00;    // 8 bits
     dma->channel[2].config.memInc = 1;      // increment destination (memory)
-    dma->channel[2].config.perInc =
-        0; // don't increment source (peripheral) address
+    dma->channel[2].config.perInc = 0;      // don't increment source
+                                            // (peripheral) address
     dma->channel[2].config.circular = 0; // not circular
     dma->channel[2].config.dir = 0;      // peripheral to memory
     dma->channel[2].config.teie = 1;     // interrupt on error
@@ -106,42 +135,69 @@ public:
 
     uart->ctrl[2] |= 0x0040; // set DMAR bit to enable DMA for receiver
     uart->request |= 0x0008; // Clear RXNE flag
-    dma->channel[1].config.enable = 1; // go!
+    dma->channel[2].config.enable = 1; // go!
 
     rx_in_progress = true;
 
     return true;
   }
 
+  void stopRX() {
+    if (isRxInProgress()) {
+      dma->channel[1].config.enable = 0;
+      // TODO thread safety
+      rx_in_progress = 0;
+    }
+  }
+
+  // Sets up an interrupt on matching char incomming form UART3
+  void interruptOnChar(char c) {
+    uart->ctrl[0] =
+        0x0008; // TODO Disable reader in order to enable characted detection
+                //    uart->ctrl[1].addr = stopChar; // TODO
+    uart->ctrl[0] = 0x000D; // TODO enable receiver and Character match
+                            // interrupt
+  }
+
   // called from DMA1_CH3_ISR
-  void onRxDmaComplete() {
+  void onDmaRxComplete() {
     // Disable DMA channel
     dma->channel[2].config.enable = 0;
     rx_in_progress = false;
+    eventListener.onRxComplete();
   }
 
   // called from DMA1_CH2_ISR
-  void onTxDmaComplete() {
+  void onDmaTxComplete() {
     // Disable DMA channel
     dma->channel[1].config.enable = 0;
     tx_in_progress = false;
+    eventListener.onTxComplete();
   }
 
   // called from UART3_ISR
   void onRxCharacterMatch() {
-    // Disable RX DMA channel if it was set up
-    if (rx_in_progress) {
-      dma->channel[2].config.enable = 0;
-      rx_in_progress = false;
-    }
+    // TODO disable interrupt
+    eventListener.onCharacterMatch();
   }
 };
 
-UART3_DMA dmaUart = UART3_DMA();
+class DummyListener : public UART_DMA_Listener {
+public:
+  // Called on DMA RX complete
+  void onRxComplete() {}
+  // Called on DMA TX complete
+  void onTxComplete() {}
+  // Called on specified character reception
+  void onCharacterMatch() {}
+};
 
-void DMA1_CH2_ISR() { dmaUart.onTxDmaComplete(); }
+DummyListener listener = DummyListener();
+UART3_DMA dmaUart = UART3_DMA(listener);
 
-void DMA1_CH3_ISR() { dmaUart.onRxDmaComplete(); }
+void DMA1_CH2_ISR() { dmaUart.onDmaTxComplete(); }
+
+void DMA1_CH3_ISR() { dmaUart.onDmaRxComplete(); }
 
 inline bool isCharacterMatchInterrupt() {
   // TODO
